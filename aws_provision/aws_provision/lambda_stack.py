bcb6d82c4e7f9916aa4ab2141239bd7532b2b21a
sunidhigup <87830404+sunidhigup@users.noreply.github.com>
2022-08-25 16:44:20 +0530

    Update lambda_stack.py

--- aws_provision/aws_provision/lambda_stack.py
+++ aws_provision/aws_provision/lambda_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core
-from constant.constant import IAMROLES
+from provision.resources.iam_job_role import JobIAmRole
 from provision.resources.job_lambda import JobLambda
 from constant.constant import STACKS_CONSTANTS
 
@@ -22,10 +22,11 @@ class Dep_lambda_stack(core.Stack):
         super().__init__(scope, id, env=env)
         config = kwargs[STACKS_CONSTANTS.CONFIG]
         self.cfg = config
-    
+        
         lambda_path= self.node.try_get_context(self.cfg)[STACKS_CONSTANTS.LAMBDA_CONFIG]
+        role_path = self.node.try_get_context(self.cfg)[STACKS_CONSTANTS.IAM_CONFIG]
 
         # Creating resources
-        roles = IAMROLES.ROLES
+        roles = JobIAmRole.createAll(self, role_path)
         # Creating resources
         lambda_functions= JobLambda.createAll(self,lambda_path,roles)
