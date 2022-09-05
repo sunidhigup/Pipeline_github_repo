06ca60889356759c8fe8c267fcf53a9a5d155805
sunidhigup <87830404+sunidhigup@users.noreply.github.com>
2022-09-05 12:49:18 +0530

    Update app.py

--- aws_provision/app.py
+++ aws_provision/app.py
@@ -8,7 +8,7 @@ from aws_provision.S3_stack import S3_Stack
 # from aws_provision.aws_provision_stack import AwsProvisionStack
 from aws_provision.Infra_stack import Dep_infra_Stack
 from aws_provision.lambda_stack import Dep_lambda_stack
-#from aws_provision.ecs_stack import Dep_ECS_Stack
+from aws_provision.ecs_stack import Dep_ECS_Stack
 from aws_provision.stepfunction import Dep_stepfunction_Stack
 from aws_provision.vpc_stack import Dep_vpc_Stack
 from aws_provision.kinesis_stack import Dep_kinesis_Stack
@@ -31,7 +31,7 @@ Dep_DB_Stack(app, "dep-db-qa", env=env_dev_us, config="dev")
 Dep_infra_Stack(app, "dep-infra-qa", env=env_dev_us, config="dev")
 S3_Stack(app, "dep-s3-qa", env=env_dev_us, config="dev")
 Dep_lambda_stack(app, "dep-lambda-stack", env=env_dev_us, config="dev")
-#Dep_ECS_Stack(app, "dep-ecs-stack", env=env_dev_us, config="dev")
+Dep_ECS_Stack(app, "dep-ecs-stack", env=env_dev_us, config="dev")
 Dep_stepfunction_Stack(app, "dep-stepfunction-stack", env=env_dev_us, config="dev")
 Dep_vpc_Stack(app, "dep-vpc-stack", env=env_dev_us, config="dev")
 Dep_kinesis_Stack(app, "dep-kinesis-stack", env=env_dev_us, config="dev")
